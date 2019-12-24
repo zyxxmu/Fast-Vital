71f086c76fb72d49d6481c7ff2d56c9a672eda72
zyxxmu <yxzhangxmu@163.com>
2019-12-24 15:43:10 +0800

    add fine-tune random_prune

--- modules/model_random_prune.py
+++ modules/model_random_prune.py
@@ -8,7 +8,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import random
+
 from .nnUtils_for_bnn import BinLinear,BinConv2d
+
+
 
 
 def append_params(params, module, prefix):
@@ -42,11 +46,43 @@ def set_optimizer(model, lr_base, lr_mult, train_all=False, momentum=0.9, w_deca
     optimizer = optim.SGD(param_list, lr = lr, momentum=momentum, weight_decay=w_decay)
     return optimizer
 
+class VGG_RandomPrune(nn.Module):
+    def __init__(self, Prunerate=1):
+        super(VGG_RandomPrune, self).__init__()
+        self.Prunerate = Prunerate
+        self.layers = self._make_layers([64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'])
+        self.classifier = nn.Linear(int(512 * Prunerate), 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+        
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        index = 0
+        Mlayers = 0
+        for x_index, x in enumerate(cfg):
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                Mlayers += 1
+            else:
+                x = int(x * self.Prunerate)
+                if x == 0:
+                    x = 1
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
 
 
-class MDNet_RandomPrune(nn.Module):
+class VGG_Vital(nn.Module):
     def __init__(self, model_path=None, K=1, Prunerate=1):
-        super(MDNet, self).__init__()
+        super(MDNet_RandomPrune, self).__init__()
         self.K = K
         self.Prunerate=Prunerate
         self.layers = nn.Sequential(OrderedDict([
@@ -58,12 +94,175 @@ class MDNet_RandomPrune(nn.Module):
                                         nn.ReLU(inplace=True),
                                         nn.LocalResponseNorm(2),
                                         nn.MaxPool2d(kernel_size=3, stride=2))),
-                ('conv3', nn.Sequential(nn.Conv2d(int(256*Prunerate), int(512*Prunerate), kernel_size=3, stride=1),
+                ('conv3', nn.Sequential(nn.Conv2d(int(256*Prunerate), 512, kernel_size=3, stride=1),
                                         nn.ReLU(inplace=True))),
-                ('fc4',   nn.Sequential(nn.Linear(int(512 * 3 * 3*Prunerate), int(512*Prunerate)),
+                ('fc4',   nn.Sequential(nn.Linear(512 * 3 * 3, 512),
                                         nn.ReLU(inplace=True))),
                 ('fc5',   nn.Sequential(nn.Dropout(0.5),
-                                        nn.Linear(int(512*Prunerate), 512),
+                                        nn.Linear(512, 512),
+                                        nn.ReLU(inplace=True)))]))
+
+        self.branches = nn.ModuleList([nn.Sequential(nn.Dropout(0.5),
+                                                     nn.Linear(512, 2)) for _ in range(K)])
+
+        for m in self.layers.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0.1)
+        for m in self.branches.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+        if model_path is not None:
+            if os.path.splitext(model_path)[1] == '.pth':
+                self.load_model(model_path)
+            elif os.path.splitext(model_path)[1] == '.pt':
+                self.load_vgg_model(model_path)
+            elif os.path.splitext(model_path)[1] == '.mat':
+                self.load_mat_model(model_path)
+            else:
+                raise RuntimeError('Unkown model format: {:s}'.format(model_path))
+        self.build_param_dict()
+
+    def build_param_dict(self):
+        self.params = OrderedDict()
+        for name, module in self.layers.named_children():
+            append_params(self.params, module, name)
+        for k, module in enumerate(self.branches):
+            append_params(self.params, module, 'fc6_{:d}'.format(k))
+
+    def set_learnable_params(self, layers):
+        for k, p in self.params.items():
+            if any([k.startswith(l) for l in layers]):
+                p.requires_grad = True
+            else:
+                p.requires_grad = False
+
+    def get_learnable_params(self):
+        params = OrderedDict()
+        for k, p in self.params.items():
+            if p.requires_grad:
+                params[k] = p
+        return params
+    
+    def get_all_params(self):
+        params = OrderedDict()
+        for k, p in self.params.items():
+            params[k] = p
+        return params
+
+    def forward(self, x, k=0, in_layer='conv1', out_layer='fc6'):
+        # forward model from in_layer to out_layer
+        run = False
+        for name, module in self.layers.named_children():
+            if name == in_layer:
+                run = True
+            if run:
+                x = module(x)
+                if name == 'conv3':
+                    x = x.view(x.size(0), -1)
+                if name == out_layer:
+                    return x
+
+        x = self.branches[k](x)
+        if out_layer=='fc6':
+            return x
+        elif out_layer=='fc6_softmax':
+            return F.softmax(x, dim=1)
+
+    def load_model(self, model_path):
+        states = torch.load(model_path)
+        shared_layers = states['shared_layers']
+        self.layers.load_state_dict(shared_layers)
+
+    def load_vgg_model(self, model_path):
+        states = torch.load(model_path)
+        filterstate = {
+                        '0':96,
+                        '1':256,
+                        '2':512,
+        }
+        last_select_index = None
+        for i in range(3)：
+            oriweight = states['conv' +str(i) + '.weight']
+            oribias = states['conv'+ str(i) + '.bias']
+
+            select_index = random.sample(range(0, filterstate[str(i)]-1), int(filterstate[str(i)]*self.Prunerate))
+            select_index.sort()
+            if last_select_index is not None:
+                for index_k, k in enumerate(select_index):
+                    for index_j, j in enumerate(last_select_index):
+                        self.layers[i][0].weight.data[index_k][index_j] = oriweight[i][j]
+            else:
+                #print(self.layers[i][0].weight.data.size())
+                #print(oriweight.size())
+                for index_k, k in enumerate(select_index):
+                    self.layers[i][0].weight.data[index_k] = oriweight[i]
+
+            last_select_index = select_index
+
+            for index_j, j in enumerate(select_index):
+                self.layers[i][0].bias.data[index_j] = oribias[j]
+
+
+
+    def load_mat_model(self, matfile):
+        mat = scipy.io.loadmat(matfile)
+        mat_layers = list(mat['layers'])[0]
+        filterstate = {
+                        '0':96,
+                        '1':256,
+                        '2':512,
+        }
+        last_select_index = None
+
+        # copy conv weights
+        for i in range(3):
+            weight, bias = mat_layers[i * 4]['weights'].item()[0]
+            oriweight = torch.from_numpy(np.transpose(weight, (3, 2, 0, 1)))
+            oribias = torch.from_numpy(bias[:, 0])
+            select_index = random.sample(range(0, filterstate[str(i)]-1), int(filterstate[str(i)]*self.Prunerate))
+            select_index.sort()
+            if last_select_index is not None:
+                for index_k, k in enumerate(select_index):
+                    for index_j, j in enumerate(last_select_index):
+                        self.layers[i][0].weight.data[index_k][index_j] = oriweight[i][j]
+            else:
+                #print(self.layers[i][0].weight.data.size())
+                #print(oriweight.size())
+                for index_k, k in enumerate(select_index):
+                    self.layers[i][0].weight.data[index_k] = oriweight[i]
+
+            last_select_index = select_index
+
+            for index_j, j in enumerate(select_index):
+                self.layers[i][0].bias.data[index_j] = oribias[j]
+
+
+
+
+
+class MDNet_RandomPrune(nn.Module):
+    def __init__(self, model_path=None, K=1, Prunerate=1):
+        super(MDNet_RandomPrune, self).__init__()
+        self.K = K
+        self.Prunerate=Prunerate
+        self.layers = nn.Sequential(OrderedDict([
+                ('conv1', nn.Sequential(nn.Conv2d(3, int(96*Prunerate), kernel_size=7, stride=2),
+                                        nn.ReLU(inplace=True),
+                                        nn.LocalResponseNorm(2),
+                                        nn.MaxPool2d(kernel_size=3, stride=2))),
+                ('conv2', nn.Sequential(nn.Conv2d(int(96*Prunerate), int(256*Prunerate), kernel_size=5, stride=2),
+                                        nn.ReLU(inplace=True),
+                                        nn.LocalResponseNorm(2),
+                                        nn.MaxPool2d(kernel_size=3, stride=2))),
+                ('conv3', nn.Sequential(nn.Conv2d(int(256*Prunerate), 512, kernel_size=3, stride=1),
+                                        nn.ReLU(inplace=True))),
+                ('fc4',   nn.Sequential(nn.Linear(512 * 3 * 3, 512),
+                                        nn.ReLU(inplace=True))),
+                ('fc5',   nn.Sequential(nn.Dropout(0.5),
+                                        nn.Linear(512, 512),
                                         nn.ReLU(inplace=True)))]))
 
         self.branches = nn.ModuleList([nn.Sequential(nn.Dropout(0.5),
@@ -153,15 +352,19 @@ class MDNet_RandomPrune(nn.Module):
             weight, bias = mat_layers[i * 4]['weights'].item()[0]
             oriweight = torch.from_numpy(np.transpose(weight, (3, 2, 0, 1)))
             oribias = torch.from_numpy(bias[:, 0])
-            select_index = random.sample(range(0, filterstate[i]-1), int(filterstate[i]*self.Prunerate))
+            select_index = random.sample(range(0, filterstate[str(i)]-1), int(filterstate[str(i)]*self.Prunerate))
             select_index.sort()
             if last_select_index is not None:
                 for index_k, k in enumerate(select_index):
                     for index_j, j in enumerate(last_select_index):
                         self.layers[i][0].weight.data[index_k][index_j] = oriweight[i][j]
             else:
+                #print(self.layers[i][0].weight.data.size())
+                #print(oriweight.size())
                 for index_k, k in enumerate(select_index):
                     self.layers[i][0].weight.data[index_k] = oriweight[i]
+
+            last_select_index = select_index
 
             for index_j, j in enumerate(select_index):
                 self.layers[i][0].bias.data[index_j] = oribias[j]
--- pretrain/RandomPrune_train_mdnet.py
+++ pretrain/RandomPrune_train_mdnet.py
@@ -9,11 +9,126 @@ import torch
 
 sys.path.insert(0,'.')
 from data_prov import RegionDataset
-from modules.model import MDNet, set_optimizer, BCELoss, Precision
+from datasets.data import cifar10
+from modules.model_random_prune import VGG_RandomPrune, VGG_Vital, set_optimizer, BCELoss, Precision
+
+device = torch.device(f"cuda:{args.gpus[0]}") if torch.cuda.is_available() else 'cpu'
+loss_func = nn.CrossEntropyLoss()
+
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
+#load pre-train params
+def load_vgg_random_model(model):
+    #print(ckpt['state_dict'])
+    origin_model = VGG_RandomPrune(prune_rate=1).to(device)
+    ckpt = torch.load(args.honey_model, map_location=device)
+    origin_model.load_state_dict(ckpt['state_dict'])
+    oristate_dict = origin_model.state_dict()
+
+    state_dict = model.state_dict()
+    last_select_index = None #Conv index selected in the previous layer
+
+    for name, module in model.named_modules():
+
+        if isinstance(module, nn.Conv2d):
+
+            oriweight = oristate_dict[name + '.weight']
+            curweight = state_dict[name + '.weight']
+            orifilter_num = oriweight.size(0)
+            currentfilter_num = curweight.size(0)
+
+            if orifilter_num != currentfilter_num and (random_rule == 'random_pretrain' or random_rule == 'l1_pretrain'):
+
+                select_num = currentfilter_num
+                if random_rule == 'random_pretrain':
+                    select_index = random.sample(range(0, orifilter_num-1), select_num)
+                    select_index.sort()
+                else:
+                    l1_sum = list(torch.sum(torch.abs(oriweight), [1, 2, 3]))
+                    select_index = list(map(l1_sum.index, heapq.nlargest(currentfilter_num, l1_sum)))
+                    select_index.sort()
+                if last_select_index is not None:
+                    for index_i, i in enumerate(select_index):
+                        for index_j, j in enumerate(last_select_index):
+                            state_dict[name + '.weight'][index_i][index_j] = \
+                                oristate_dict[name + '.weight'][i][j]
+                else:
+                    for index_i, i in enumerate(select_index):
+                        state_dict[name + '.weight'][index_i] = \
+                            oristate_dict[name + '.weight'][i]
+
+                last_select_index = select_index
+
+            else:
+                state_dict[name + '.weight'] = oriweight
+                last_select_index = None
+
+    model.load_state_dict(state_dict)
+
+# Training
+def train(model, optimizer, trainLoader, opts, epoch):
+
+    model.train()
+    for batch, (inputs, targets) in enumerate(trainLoader):
+
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        output = model(inputs)
+        loss = loss_func(output, targets)
+        loss.backward()
+        optimizer.step()
+
+
+
+
+#Testinga
+def test(model, testLoader):
+    global best_acc
+    model.eval()
+
+    losses = utils.AverageMeter()
+    accurary = utils.AverageMeter()
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testLoader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = loss_func(outputs, targets)
+
+            losses.update(loss.item(), inputs.size(0))
+            predicted = utils.accuracy(outputs, targets)
+            accurary.update(predicted[0], inputs.size(0))
+
+    return accurary.avg
+
+
 def train_mdnet(opts):
+
+    loader = cifar10.Data(args)
+    best_acc = 0.0
+
+    model_vgg = VGG_RandomPrune(opts['prune_rate']).to(device)
+    load_vgg_random_model(model_vgg)
+
+
+    optimizer = optim.SGD(model.parameters(), lr=opts['lr_finetune'], momentum=opts['momentum'], weight_decay=opts['weight_decay'])
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=opts['lr_decay_step'], gamma=0.1)
+
+    for epoch in range(opts['num_epochs']):
+        train(model, optimizer, loader.trainLoader, opts, epoch)
+        scheduler.step()
+        test_acc = test(model, loader.testLoader)
+
+        is_best = best_acc < test_acc
+        best_acc = max(best_acc, test_acc)
+
+    
+    model_state_dict = model.module.state_dict() if len(opts[gpus]) > 1 else model.state_dict()
+    torch.save(model_state_dict, opts['pruned_model_path'])
+    print('Best accurary: {:.3f}'.format(float(best_acc)))
+
+
 
     # Init dataset
     with open(opts['data_path'], 'rb') as fp:
@@ -25,7 +140,7 @@ def train_mdnet(opts):
         dataset[k] = RegionDataset(seq['images'], seq['gt'], opts)
 
     # Init model
-    model = MDNet_RandomPrune(opts['init_model_path'], K, 0.8)
+    model = VGG_Vital(opts['pruned_model_path'], K, opts['prune_rate'])
     if opts['use_gpu']:
         model = model.cuda()
     model.set_learnable_params(opts['ft_layers'])
@@ -91,4 +206,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     opts = yaml.safe_load(open('pretrain/options_{}.yaml'.format(args.dataset), 'r'))
+    
     train_mdnet(opts)
